2b6b963650ce2ffdc2472e649de20d5f0de89741
Falker34 <70699814+Falker34@users.noreply.github.com>
2020-10-15 22:05:25 +0300

    Update fortesting.py

--- fortesting.py
+++ fortesting.py
@@ -1,9 +1,13 @@
-f = open('left.txt','r')
-g = open('dead.txt','r+')
-n = '9'
-#print(f.read())
-#print(f.readline(4))
-print(f.read().split('\n'))
-g.write(n)
-g.close()
-f.close()
+#Series26
+#K = int(input("Введите число K: ")) # два числа мне удалось перенести из файла
+#N = int(input("Введите число N: "))
+f = open('left.txt','r') #файл ввода
+g = open('dead.txt','w') #файл вывода
+N = f.read(1)
+N = int(N) #перевожу элемент из строки в число
+K = f.read(2)
+K = int(K)
+Kna = []
+Kna.append(f.read()) # вся строчка выходит как одно число
+#Kna.remove('\n') # неудачная попытка убрать "\n"
+print(N,K,Kna)
